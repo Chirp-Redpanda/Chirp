73bd23ed75695d831f754d682404977723a82db7
Joyce Dai <113967795+jingyued@users.noreply.github.com>
2024-05-16 23:48:12 -0700

    Delete chirp-backend directory

--- chirp-backend/dist/chirrup/dto/update-chirrup.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateChirrupDto } from './create-chirrup.dto';
-declare const UpdateChirrupDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateChirrupDto>>;
-export declare class UpdateChirrupDto extends UpdateChirrupDto_base {
-}
-export {};
--- chirp-backend/src/chirrup/dto/update-chirrup.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChirrupDto } from './create-chirrup.dto';
-
-export class UpdateChirrupDto extends PartialType(CreateChirrupDto) {}
